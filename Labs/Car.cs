3b4a2e2251a27df1bebcc5003f4bc605dcd511cc
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-03-10 22:33:14 +0200

    ✨: add some examples to Lab1

---
+++ Labs/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs
+{
+    public class Car
+    {
+        private readonly string _name = "Default name";
+        private readonly int _ownerId;
+        private readonly string _color = "Default color";
+
+        public Car(int ownerId, string name, string color)
+        {
+            _ownerId = ownerId;
+            _name = name;
+            _color = color;
+        }
+
+        public override string ToString()
+        {
+            var properties = $"Owner Id - {_ownerId}, name - {_name} and color - {_color}";
+
+            return properties;
+        }
+    }
+}
--- Labs/Program.cs
+++ Labs/Program.cs
@@ -6,8 +6,61 @@ internal class Program
 {
     private static void Main()
     {
+        Car firstCar = new(1, "Toyota", "Red");
+        Car secondCar = new(2, "Nissan", "White");
+        Car thirdCar = new(3, "Honda", "Black");
 
+        Car[] cars = new Car[] { firstCar, secondCar, thirdCar };
+
+        //AddExample(cars);
+        RemoveExample(cars);
     }
 
+    private static void AddExample(Car[] cars)
+    {
+        var carsList = new CircularLinkedList<Car>();
+        carsList.Add(cars[0]);
+        carsList.AddFirst(cars[1]);
+        carsList.AddAt(cars[2], 1);
 
+        Console.WriteLine(carsList.ToString()); // Nissan Honda Toyota
+    }
+
+    private static void RemoveExample(Car[] cars)
+    {
+        var carsList = new CircularLinkedList<Car>();
+        carsList.Add(cars[2]);
+        carsList.Add(cars[0]);
+        carsList.Add(cars[0]);
+        carsList.Add(cars[0]);
+        carsList.Add(cars[1]);
+        carsList.Add(cars[2]);
+        Console.WriteLine(carsList.ToString()); // Honda Toyota Toyota Toyota Nissan Honda
+
+        Console.WriteLine("///////");
+
+        carsList.RemoveAt(2);
+        Console.WriteLine(carsList.ToString()); // Honda Toyota Toyota Nissan Honda
+
+        Console.WriteLine("///////");
+
+        carsList.Remove(cars[1]);
+        Console.WriteLine(carsList.ToString()); // Honda Toyota Toyota Honda
+
+        Console.WriteLine("///////");
+
+        carsList.RemoveHead();
+        Console.WriteLine(carsList.ToString()); // Toyota Toyota Honda
+
+        Console.WriteLine("///////");
+
+        carsList.RemoveTail();
+        Console.WriteLine(carsList.ToString()); // Toyota Toyota
+
+        Console.WriteLine("///////");
+
+        carsList.RemoveAll(cars[0]);
+        Console.WriteLine(carsList.ToString()); // -_-
+        Console.WriteLine("///////");
+    }
 }
