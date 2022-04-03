aeb51ef988d8a274e07e1fbd98554f0f33403ac6
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-04-03 15:06:47 +0300

    🎉: start lab3

---
+++ Lab3/HotelBooking/HotelBooking/Entities/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelBooking.DAL.Entities;
+
+public class Room
+{
+    public int Id { get; set; }
+    public int HotelId { get; set; }
+    public string Name { get; set; } = "Default Name";
+    public int Price { get; set; }
+    public int Type { get; set; }
+    public int Capacity { get; set; }
+}
