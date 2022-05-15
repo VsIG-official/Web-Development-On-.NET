340ccff15fe6c57e785c2dafcc971ea944267fce
Valentyn-Dominskyi <valentyn2202@gmail.com>
2022-05-15 20:06:10 +0300

    🎉: start lab3 again

---
+++ Lab3/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab3.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        T Get(int id);
+        void Update(T item);
+        void Delete(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Func<T, bool> predicate);
+    }
+}
